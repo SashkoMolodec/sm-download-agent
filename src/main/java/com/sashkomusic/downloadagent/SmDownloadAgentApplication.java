c48ef0b2393e15cb826bf096a6cb942484e7198d
Sasha <sanykk2014@gmail.com>
2025-11-19 01:36:47 +0200

    add more generated files

---
+++ src/main/java/com/sashkomusic/downloadagent/SmDownloadAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sashkomusic.downloadagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmDownloadAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmDownloadAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sashkomusic/downloadagent/SmDownloadAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashkomusic.downloadagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmDownloadAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
